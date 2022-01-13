c499559b1a3d271b27151cdb7fed9296f359cc8a
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-01-13 13:52:14 +0100

    Update start.js

--- start.js
+++ start.js
@@ -1,11 +1,11 @@
 var pass = "kle12"
 var user = prompt();
-if(user==user)
+if(pass==user)
 {
-  alert(Access granted)
+  alert("Access granted");
 } else
 {
-  alert(Access denied)
+  alert("Access denied");
   window.location("https://www.google.com")
   
 }
