80cac9029a13bdc854967322514e3e39d3919ca2
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-01-27 14:26:33 +0100

    Add files via upload

---
+++ games/table/gameplay2.php
@@ -0,0 +1,50 @@
+<html class="no-js play" prefix="og: http://ogp.me/ns#" data-type="play">
+<head> 
+<meta charset="UTF-8" />
+<link rel="canonical" href="https://www.spelo.com/spel5937.htm"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+<title>Table Tennis World Tour</title>
+<style>
+.html, body{
+  min-height: 100%;
+}
+.body{
+  position: relative;
+}
+.overlay{
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 82px;
+  height: 25px;
+  z-index: 10;
+  background-color: black; /*dim the background*/
+font-family: "Arial Black", Gadget, sans-serif;
+font-size: 16px;
+color: #FFFFFF;
+letter-spacing: -0.6px;
+word-spacing: 0px;
+font-weight: 600;
+text-decoration: none solid rgb(68, 68, 68);
+font-style: normal;
+font-variant: normal;
+text-transform: none;
+}
+</style>
+ <style>
+    a { color: #FFFFFF; } /* CSS link color */
+  </style>
+<style>
+.play object.html5-game,.play iframe.html5-game{margin:0;padding:0;border:0;background:#000;display:block;z-index:1;position:fixed;width:100%;height:100%;top:0;left:0}.play.preloader .html5-game{display:none}.play .page{min-height:0 !important;padding:0 !important;margin:0 !important}.mobile-ad-wrapper{display:none;position:absolute;left:0;top:0;right:0;bottom:0;z-index:3000;background:#000;text-align:center}.mobile-ad-wrapper
+</style>
+</head> 
+<body>
+<object id="gameSource" class="html5-game" scrolling="no" height="100%" width="100%" frameborder="0" seamless="true" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true" webkit-playsinline="true" data="https://play.famobi.com/table-tennis-world-tour/A-Z4DOI"></object>
+
+
+
+
+</body>
+
+
+</html>
