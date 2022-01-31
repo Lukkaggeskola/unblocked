4763f399cf9e4bac17077729542718a1d0617d85
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-01-31 11:18:19 +0100

    Add files via upload

---
+++ games/tetris/if_game_html5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="renderer" content="webkit" />
+<meta name="force-rendering" content="webkit" />
+<style>body,canvas,div{outline:none;-moz-outline-style:none;-moz-outline-style:none;-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none;-khtml-user-select:none;-webkit-tap-highlight-color:rgba(0,0,0,0)}body{margin:0;padding:0;border:0;overflow:hidden}canvas{background-color:rgba(0,0,0,0)}#contentDiv{display:table;margin:auto}#GameDiv{position:relative;width:800px;height:600px;left:0;top:0;background:#000;outline:none;-moz-outline-style:none}#Cocos2dGameContainer{position:absolute;margin:0;overflow:hidden;left:0;top:0}#loadingText{position:absolute;top:50%;left:50%;width:116px;height:116px;margin-top:-50px;margin-left:-50px;border:0 solid #000;color:#eee;font-family:Verdana;font-size:16px;text-align:center;vertical-align:middle;line-height:116px}#loadingDisplay{position:absolute;top:50%;left:50%;width:100px;height:100px;margin-top:-50px;margin-left:-50px;border:8px solid #eee;border-top:8px solid #666;border-radius:50%;animation:spin 1.5s linear infinite}@keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } }</style>
+</head>
+<body>
+
+<div id="contentDiv">
+<div id="GameDiv">
+<canvas id="GameCanvas"></canvas>
+</div>
+<div id="loadingDisplay"></div>
+<div id="loadingText">LOADING</div>
+</div>
+
+<script type="adede0b83ddb769317d72ff2-text/javascript">var t=1643623961;var isIFrame=window.frameElement&&(window.frameElement.nodeName=="IFRAME");if(('true'=='1')&&!isIFrame){window.location.replace(window.location.protocol+"//"+window.location.hostname);}else{var pageStartTimeMSEC=Date.now();var cbidg='C24E80D3C7A8B26E';var isDesktop='1'=='1';var isMobile=''=='1';window.gameContentBasePath='';window.platformSrcDir='src-desktop/';var gameDiv=document.getElementById("GameDiv");var gameCanvas=document.getElementById("GameCanvas");var settingsJS=document.createElement('script');settingsJS.async=false;settingsJS.src=window.platformSrcDir+"settings.js?cbidg="+getCBID();document.body.appendChild(settingsJS);var mainJS=document.createElement('script');mainJS.async=false;mainJS.src=window.gameContentBasePath+"main-bps.js?cbidg="+getCBID();document.body.appendChild(mainJS);function getPageElapsedTimeMSEC(){return isIFrame?window.parent.getPageElapsedTimeMSEC():((Date.now()-pageStartTimeMSEC));}function getCBID(){return cbidg;}function getGameDiv(){return gameDiv;}function getGameCanvas(){return gameCanvas;}function gameLoadingSceneIsReady(){try{removeLoadingDisplay();}catch(e){}}function removeLoadingDisplay(){{var loadingText=document.getElementById('loadingText');if(loadingText){loadingText.parentNode.removeChild(loadingText);}{var loadingDisplay=document.getElementById('loadingDisplay');if(loadingDisplay){loadingDisplay.parentNode.removeChild(loadingDisplay);}}}}function trackEvent(category,action,opt_label,opt_value,opt_noninteraction){if(isIFrame){try{window.parent.trackEvent(category,action,opt_label,opt_value,opt_noninteraction);}catch(e){}}}function refreshAds(){if(isIFrame){window.parent.refreshAds();}}function isGameAreaAdEnabled(){return isIFrame?window.parent.canShowAds:false;}function isGameAreaAdActive(){return isIFrame?window.parent.isGameAreaAdActive:false;}function showGameAreaAd(pType='next'){if(isIFrame){window.parent.showGameAreaAd();}}function onGameAreaAdComplete(){var onGameAreaAdCompleteInterval=setInterval(function(){if(window.mBPSApp){clearInterval(onGameAreaAdCompleteInterval);window.parent.document.activeElement.blur();window.parent.document.getElementById("gameIFrame").focus();document.activeElement.blur();document.getElementById("GameCanvas").focus();window.mBPSApp.dispatchAppMessage(979287055);}},100);}function axGAFG2021_beforeAx(){}function axGAFG2021_afterAx(){}function axGAFG2021_axBreakDone(pPlacementInfo){onGameAreaAdComplete();}}</script>
+<script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="adede0b83ddb769317d72ff2-|49" defer=""></script></body>
+</html>
