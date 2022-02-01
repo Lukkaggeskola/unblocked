3b34e264a26860505c2f4c860427fcda6767ad71
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-02-01 12:54:23 +0100

    Update start.js

--- start.js
+++ start.js
@@ -3,15 +3,9 @@ var pass2 = "mlk"
 var user = prompt("Enter access code");
 if(pass==user)
 {
-} else
-{
-  
-  window.location = "https://www.kaggesite.ml/denied";
-  
-}
-if(pass2==user)
+} else if(pass2==user)
 {
 }else
 {
-  window.location = "https://www.kaggesite.ml/denied";
+  window.location = "https://www.kaggesite.ml/denied"
 }
