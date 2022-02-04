d08e10fc371e4f7f4a5303167434517146672b26
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-02-04 17:23:19 +0100

    Create VIPcode.js

---
+++ VIPcode.js
@@ -0,0 +1,25 @@
+var code1 = "kle12vip12"
+var code2 = "vip12"
+var code3 = "12"
+var user1 = prompt("Enter code 1")
+var user2 = prompt("Enter code 2")
+var user3 = prompt("Enter code 3")
+if(user1==code1)
+{
+}else
+{
+  window.location="https://kaggesite.ml/denied.html"
+}
+if(user2==code2)
+{
+}else
+{
+  window.location="https://kaggesite.ml/denied.html"
+}
+if(user3==code3)
+{
+  window.location="https://kaggesite.ml/a"
+}else
+{
+  window.location="https://kaggesite.ml/denied.html"
+}
