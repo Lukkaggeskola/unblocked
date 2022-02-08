701b5930b11a35bc8e73b223aa0cd4468f16d208
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-02-08 13:18:23 +0100

    Create ög.js

---
+++ users/ög.js
@@ -0,0 +1,11 @@
+function ög(){
+var user = "ford ranger kle12"
+var userr = prompt()
+if(user==userr)
+{
+  window.location = "https://kaggesite.ml/VIP.html"
+}else
+{
+  window.location = "https://kaggesite.ml/denied.html
+}
+}
