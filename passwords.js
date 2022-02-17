6c3d52348ca4597a391754412447e2075bf672c8
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-02-17 16:34:38 +0100

    Update passwords.js

--- passwords.js
+++ passwords.js
@@ -1,1 +1,223 @@
 
+function klll()
+		  {
+		  var uhuhyhu = "12345"
+		  var ughgegf = prompt()
+		  if(uhuhyhu==ughgegf)
+		  {
+			  window.location = "https://kagge.gq/why.html"
+		  }else
+		  {
+			  window.location = "https://kagge.gq/denied.html"
+		  }
+		  }
+ function eeeee(){
+		  var okig = "anders"
+		  var ushg = prompt()
+		  if(okig==ushg)
+		  {
+			  alert("code 1 : kle12vip12")
+			  alert("code 2 : vip12")
+			  alert("code 3 : 12")
+			  alert("access code: kle12")
+			  window.location = "https://kagge.gq/VIP"
+		  }else
+			  {
+				  window.location = "https://kagge.gq/denied.html"
+			  }
+		  }
+function erikaa()
+			  {
+				  var passeif = "kan du inte cykla ellor"
+				  var usergbj = prompt()
+				  if(usergbj==passeif)
+				  {
+					  window.location = "https://kagge.gq/why.html"
+				  }else
+				  {
+					  window.location = "https://kagge.gq/denied.html"
+				  }
+function biver()
+		  {
+			  var passbu = "suka blyat"
+			  var usrinuu = prompt()
+			  if(passbu==usrinuu)
+			  {
+				window.location = "https://kagge.gq/why.html"  
+			  }else
+			  {
+				  window.location = "https://kagge.gq/denied.html"
+				  
+			  }
+		  }
+ function devin()
+		  {
+			  var devivpas = "devinlito"
+			  var userkek = prompt()
+			  if(userkek==devivpas)
+			  {
+				  
+				  window.location = "https://kagge.gq/why.html"
+				  
+			  }else
+			  {
+				  window.location = "https://kagge.gq/denied.html"
+			  }
+		  }
+  function jhoann(){
+		  var jhopassuy = "neincraft"
+		  var userjhouy = prompt()
+		  if(jhopassuy==userjhouy)
+		  {
+			  window.location = "https://kagge.gq/why.html"
+			 
+		  }else
+		  {
+			  window.location = "https://kagge.gq/denied.html"
+		  }
+		  }
+function startguest()
+      {
+        alert("Starting guest mode. Press ok to continue")
+        window.location = "https://kagge.gq/guest.html"
+      }
+function tildec()
+		  {
+			  var upas = "hejsan100"
+			  var userinj = prompt()
+			  if(upas==userinj)
+			  {
+				  window.location = "https://kagge.gq/why.html"
+			  }else
+			  {
+				  window.location = "https://kagge.gq/denied.html"
+			  }
+		  }
+function leouu()
+		  {
+			  var passleo = "atombomb"
+			  var userleoo = prompt()
+			  if(userleoo==passleo)
+			  {
+				  window.location = "https://kagge.gq/why.html"
+				 
+			  }else
+			  {
+				  window.location = "https://kagge.gq/denied.html"
+			  }
+		  }
+  function arre()
+		  {
+			  var passarre = "arvids25"
+			  var userinar = prompt()
+			  if(userinar==passarre)
+			  {
+				  window.location = "https://kagge.gq/why.html"
+			  }else
+			  {
+				  windoow.location = "https://kagge.gq/denied.html"
+			  }
+		  }
+  function fili()
+		  {
+			  var passh = "korv"
+			  var userrr = prompt()
+			  if(userrr==passh)
+			  {
+				  window.location = "https://kagge.gq/why.html"
+			  }else
+			  {
+				  window.location = "https://kagge.gq/denied.html"
+			  }
+		  }
+ function kel(){
+      var passkel = "Kelvin"
+      var usin = prompt()
+      if(passkel==usin)
+      {
+	      //one month trail DISABLE ON 7 MARCH
+        window.location = "https://kagge.gq/why.html"
+      }else
+      {
+        window.location = "https://kagge.gq/denied.html"
+      }
+      }
+function no()
+        {
+         
+          var h12 = "porr009"
+          var p = prompt()
+          if(p==h12)
+          {
+           
+            window.location = "https://kagge.gq/why.html"
+          } else
+          {
+            window.location = "https://kagge.gq/denied.html"
+          }
+        }
+function noo()
+        {
+          var inuu = prompt()
+          var passög = "ford raptor"
+          if(inuu==passög)
+          {
+            
+            window.location = "https://kagge.gq/why.html"
+          }
+          else
+          {
+            window.location = "https://kagge.gq/denied.html"
+          }
+        }
+function noB()
+        {
+          var passB = "caktus"
+         var inu = prompt()
+         if(inu==passB)
+         {
+           window.location = "https://kagge.gq/why.html"
+         }else
+         {
+           window.location = "https://kagge.gq/denied.html"
+         }
+        }
+        
+function munk()
+        {
+          var munk = "anime titties"
+          var psssu = prompt()
+          if(psssu==munk)
+          {
+            window.location = "https://kagge.gq/why.html"
+          }else
+          {
+            window.location = "https://kagge.gq/denied.html"
+          }
+        }
+ function ga()
+        {
+          var pass = "31"
+          var paaa = prompt()
+          if(pass==paaa)
+          {
+            window.location = "https://kagge.gq/why.html"
+          }else
+          {
+            window.location = "https://kagge.gq/denied.html"
+          }
+          
+        }
+ function gga()
+        {
+          var us = prompt()
+          var g = " , . ," 
+          if(us==)
+          {
+            window.location = "https://kagge.gq/VIP.html"
+          }else
+          {
+            alert(";) Leave now")
+            window.location = "https://kagge.gq/denied.html"
+          }
+        
