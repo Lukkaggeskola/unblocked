dfc8735045278c1fa50d1f5a799621b0d9895a7b
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-02-17 17:02:44 +0100

    Create erik.js

---
+++ users/erik.js
@@ -0,0 +1,15 @@
+function eeeee(){
+		  var okig = "anders"
+		  var ushg = prompt()
+		  if(okig==ushg)
+		  {
+			  alert("code 1 : kle12vip12")
+			  alert("code 2 : vip12")
+			  alert("code 3 : 12")
+			  alert("access code: kle12")
+			  window.location = "https://kagge.gq/VIP"
+		  }else
+			  {
+				  window.location = "https://kagge.gq/denied.html"
+			  }
+		  }
