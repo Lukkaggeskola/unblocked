6e0be261321be57a622021e5f617a4dfac1b6f35
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-02-17 17:03:02 +0100

    Create kagge.js

---
+++ users/kagge.js
@@ -0,0 +1,13 @@
+ function gga()
+        {
+          var us = prompt()
+          var g = " , . ," 
+          if(us==)
+          {
+            window.location = "https://kagge.gq/VIP.html"
+          }else
+          {
+            alert(";) Leave now")
+            window.location = "https://kagge.gq/denied.html"
+          }
+        }
