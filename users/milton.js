748844ffc9686e3f33976a3572b62c8d9ebe623b
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-02-18 11:36:35 +0100

    Create milton.js

---
+++ users/milton.js
@@ -0,0 +1,14 @@
+function no()
+        {
+         
+          var h12 = "porr009"
+          var p = prompt()
+          if(p==h12)
+          {
+           
+            window.location = "https://kagge.gq/VIP.html"
+          } else
+          {
+            window.location = "https://kagge.gq/denied.html"
+          }
+        }
