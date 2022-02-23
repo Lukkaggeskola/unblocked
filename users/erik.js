9dda1a2c7b252a8e42172250121be3e975a58a7e
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-02-23 12:33:43 +0100

    Update erik.js

--- users/erik.js
+++ users/erik.js
@@ -7,7 +7,7 @@ function eeeee(){
 			  alert("code 2 : vip12")
 			  alert("code 3 : 12")
 			  alert("access code: kle12")
-			  window.location = "https://kagge.gq/VIP"
+			  window.location = "https://kagge.gq/kagga"
 		  }else
 			  {
 				  window.location = "https://kagge.gq/denied.html"
