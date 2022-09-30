14f2ffe1c59185c3a5fb7d4bfa4e83f2c36a325d
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-09-30 12:48:08 +0200

    Update script.js

--- games/slope/script.js
+++ games/slope/script.js
@@ -9,5 +9,16 @@ if((event.key == "q" || event.key == "Q") && event.altKey) {
   window.location = "https://hub.infomentor.se"
 }
 })
+if((event.key == "j" || event.key == "J") && event.altKey) {
+  var fps = 5;
+  function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    console.log(startTime);
+    animate();
+}
+} 
+
 
 
