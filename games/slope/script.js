a67812ed644f9f51154b1823d8cd3749bb371188
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-09-30 13:27:56 +0200

    Update script.js

--- games/slope/script.js
+++ games/slope/script.js
@@ -9,16 +9,45 @@ if((event.key == "q" || event.key == "Q") && event.altKey) {
   window.location = "https://hub.infomentor.se"
 }
 })
-if((event.key == "j" || event.key == "J") && event.altKey) {
-  var fps = 5;
-  function startAnimating(fps) {
-    fpsInterval = 1000 / fps;
-    then = Date.now();
-    startTime = then;
-    console.log(startTime);
-    animate();
+// define a handler
+function doc_keyUp(e) {
+
+    // this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
+    if (e.ctrlKey && e.key === 'ArrowDown') {
+        // call your function to do the thing
+        
+var limitLoop = function (fn, fps) {
+ 
+    // Use var then = Date.now(); if you
+    // don't care about targetting < IE9
+    var then = new Date().getTime();
+
+    // custom fps, otherwise fallback to 60
+    fps = fps || 5;
+    var interval = 1000 / fps;
+ 
+    return (function loop(time){
+        requestAnimationFrame(loop);
+ 
+        // again, Date.now() if it's available
+        var now = new Date().getTime();
+        var delta = now - then;
+ 
+        if (delta > interval) {
+            // Update time
+            // now - (delta % interval) is an improvement over just 
+            // using then = now, which can end up lowering overall fps
+            then = now - (delta % interval);
+ 
+            // call the fn
+            fn();
+        }
+    }(0));
+};
+    }
 }
-} 
+// register the handler 
+document.addEventListener('keyup', doc_keyUp, false);
 
 
 
