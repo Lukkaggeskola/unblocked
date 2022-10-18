e0c7cf34bb8636d3597d8c4affc873ad9444b550
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-10-18 18:17:07 +0200

    Update start.js

--- games/csgofree/start.js
+++ games/csgofree/start.js
@@ -1,4 +1,4 @@
-var pass =  ""
+var pass =  "BUKjier9+cn8)NIghru8sw0nyghr780HGAhug8renvfyt7480wMUCY/(=rnfy738tnhCUmMUc7rt4t834nfyt4738q9654634i689jhtr70HNGY/()Ng76840"
 var user = prompt("Enter access code");
 if(pass==user)
 {
