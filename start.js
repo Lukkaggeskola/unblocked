217da358d6a1ee54cbd251c123aadfc3d04493e7
Lukkaggeskola <82468194+Lukkaggeskola@users.noreply.github.com>
2022-10-18 17:56:45 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,11 +1,9 @@
-var pass = "kle12"
-var pass2 = "mlk"
+var pass =  ""
 var user = prompt("Enter access code");
 if(pass==user)
 {
-} else if(pass2==user)
+} else
 {
-}else
-{
-  window.location = "https://www.kagge.gq/denied"
+  prompt("wrong access code")
+  window.location = "https://www.kagge.gq/"
 }
